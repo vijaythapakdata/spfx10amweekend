f284c17d8e2d7d75182c1308cee951faeb9ae89d
vijaythapakdata <vijaythapak24@gmail.com>
2026-05-17 10:43:56 +0530

    latest updates

---
+++ src/webparts/spHttpclient/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ISpHttpclientWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'SpHttpclientWebPartStrings' {
+  const strings: ISpHttpclientWebPartStrings;
+  export = strings;
+}
